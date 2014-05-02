a6b17e3b7af990e843a9f1def0d442a2c6bfd838
poofytoo <victorhung92@gmail.com>
2014-05-02 02:21:22 -0400

    first photo is elevator

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,19 @@ $(document).ready(function(){
               0:['1.jpg', '#f2c21a', '#ffffff', '#ffffff'],
               1:['2.jpg', '#000000', '#ffffff', '#000000'],
               2:['3.jpg', '#d3f4f0', '#0e6057', '#ffffff'],
+              3:['4.jpg', '#e6e4ca', '#000000', '#ffffff'],
             }
-  var s = scheme[Math.floor(Math.random()*(Object.keys(scheme).length))];
+  
+  // var s = scheme['3'];
+
+  // ensures first visit always is the elevator photo
+  if (localStorage.getItem("visited")) {
+    var s = scheme[Math.floor(Math.random()*(Object.keys(scheme).length))];
+  } else {
+    localStorage.setItem("visited", true);
+    var s = scheme[0];
+  }
+
   $('body').css('background-image',"url('css/images/title/"+s[0]+"')");
   $('a').css('background-color',s[1]);
   $('a').css('color',s[2]);
